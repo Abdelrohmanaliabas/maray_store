668221c63cd804224c16bfb9e81c061387c8d38a
AbdelrahmanAli <abdelrahmanali2310@gmail.com>
2026-02-01 02:04:38 +0200

    edit v

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             AdminSeeder::class,
-            CatalogSeeder::class,
+            // CatalogSeeder::class,
         ]);
     }
 }
